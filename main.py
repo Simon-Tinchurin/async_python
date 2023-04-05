e068d2d4b832204e9293a4920171cebf59c2079b
sam <sementinchurin@gmail.com>
2023-04-05 13:47:15 +0300

    basic event loop

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 import socket
 import logging
+from select import select
+
+
+# to see which ports are in use: netstat -tuln
+
+to_monitor = []
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5001))
+server_socket.bind(('localhost', 5000))
 server_socket.listen()
 
 FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 DATE_FORMAT = '%d.%m.%Y %I:%M:%S %p'
-logging.basicConfig(filename='asinc_logs.log',
+logging.basicConfig(filename='async_logs.log',
                     format=FORMAT,
                     datefmt=DATE_FORMAT,
                     level=logging.INFO)
 
-while True:
-    logging.info('Before .accept()')
+
+def accept_connection(server_socket):
+    logging.info('Inside accept_connection func')
     client_socket, addr = server_socket.accept()
     logging.info(f'Connection from: {addr}')
+    to_monitor.append(client_socket)
 
+
+def send_message(client_socket):
+    logging.info('inside send_message func')
+    request = client_socket.recv(4096)
+
+    if request:
+        response = 'Something received\n'.encode()
+        logging.info(f'received {request.decode()}')
+        client_socket.send(response)
+    else:
+        logging.info(f'closing socket {client_socket}')
+        client_socket.close()
+
+
+def event_loop():
     while True:
-        request = client_socket.recv(4096)
+        # read, write, errors
+        ready_to_read, _, _ = select(to_monitor, [], [])
+        for sock in ready_to_read:
+            if sock is server_socket:
+                accept_connection(sock)
+            else:
+                send_message(sock)
 
-        if not request:
-            break
-        else:
-            response = 'Something received\n'.encode()
-            client_socket.send(response)
-    logging.info('outside inner while loop')
-    client_socket.close()
+
+if __name__ == '__main__':
+    to_monitor.append(server_socket)
+    event_loop()
