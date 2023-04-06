630d6a3fc8126ab9b81322c4fdc2a41447ef8a6d
sam <sementinchurin@gmail.com>
2023-04-06 17:10:22 +0300

    callback example

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import socket
 import logging
-from select import select
+import selectors
 
 
 # to see which ports are in use: netstat -tuln
 
-to_monitor = []
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5000))
-server_socket.listen()
+selector = selectors.DefaultSelector()
 
 FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 DATE_FORMAT = '%d.%m.%Y %I:%M:%S %p'
@@ -20,11 +15,25 @@ logging.basicConfig(filename='async_logs.log',
                     level=logging.INFO)
 
 
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 5000))
+    server_socket.listen()
+
+    selector.register(fileobj=server_socket,
+                      events=selectors.EVENT_READ,
+                      data=accept_connection)
+
+
 def accept_connection(server_socket):
     logging.info('Inside accept_connection func')
     client_socket, addr = server_socket.accept()
     logging.info(f'Connection from: {addr}')
-    to_monitor.append(client_socket)
+
+    selector.register(fileobj=client_socket,
+                      events=selectors.EVENT_READ,
+                      data=send_message)
 
 
 def send_message(client_socket):
@@ -37,20 +46,19 @@ def send_message(client_socket):
         client_socket.send(response)
     else:
         logging.info(f'closing socket {client_socket}')
+        selector.unregister(client_socket)
         client_socket.close()
 
 
 def event_loop():
     while True:
-        # read, write, errors
-        ready_to_read, _, _ = select(to_monitor, [], [])
-        for sock in ready_to_read:
-            if sock is server_socket:
-                accept_connection(sock)
-            else:
-                send_message(sock)
+        # (key, events)
+        events = selector.select()
+        for key, _ in events:
+            callback = key.data
+            callback(key.fileobj)
 
 
 if __name__ == '__main__':
-    to_monitor.append(server_socket)
+    server()
     event_loop()
