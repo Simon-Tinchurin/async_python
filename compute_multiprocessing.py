84470fe1a163c4baeb35006753164fe8d22f73b0
sam <sementinchurin@gmail.com>
2023-05-14 14:26:38 +0300

    multiprocessing

--- compute_multiprocessing.py
+++ compute_multiprocessing.py
@@ -13,21 +13,29 @@ def main():
     print(f'Doing math on {multiprocessing.cpu_count()} processors')
     processor_count = multiprocessing.cpu_count()
     pool = multiprocessing.Pool()
+    tasks = []
     for n in range(1, processor_count + 1):
-        pool.apply_async(do_math, (30000000 * (n - 1) / processor_count,
-                                   30000000 * n / processor_count))
+        task = pool.apply_async(do_math, (30000000 * (n - 1) / processor_count,
+                                          30000000 * n / processor_count))
+        tasks.append(task)
     pool.close()
     pool.join()
     dt = datetime.datetime.now() - t0
     print(f'Done in {dt.total_seconds()}')
+    print(f'Results:')
+    for t in tasks:
+        print(t.get())
 
 
 def do_math(start=0, num=10):
     pos = start
     k_sq = 1000 * 1000
+    average = 0
     while pos < num:
         pos += 1
-        math.sqrt((pos - k_sq) * (pos - k_sq))
+        val = math.sqrt((pos - k_sq) * (pos - k_sq))
+        average += val / num
+    return int(average)
 
 
 if __name__ == '__main__':
